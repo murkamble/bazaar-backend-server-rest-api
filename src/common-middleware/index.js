f20f5e57ef4693423d6c2125c1b9c51b652f27dc
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-25 15:43:02 +0530

    S3 code change

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -8,10 +8,10 @@ const shortid = require('shortid');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cd) {
+    destination: function (req, file, cd) {
         cd(null, path.join(path.dirname(__dirname), 'uploads'))
     },
-    filename: function(req, file, cd) {
+    filename: function (req, file, cd) {
         cd(null, shortid.generate() + '-' + file.originalname)
     }
 })
@@ -19,31 +19,33 @@ const storage = multer.diskStorage({
 exports.upload = multer({ storage });
 
 var s3 = new aws.S3({
-    accessKeyID: "AKIAZ22LFMUC3HVRWX2N",
+    accessKeyId: "AKIAZ22LFMUC3HVRWX2N",
     secretAccessKey: "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"
+    // aws_access_key_id: "AKIAZ22LFMUC3HVRWX2N",
+    // aws_secret_access_key: "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"
 })
 
 exports.uploadS3 = multer({
     storage: multerS3({
-      s3: s3,
-      bucket: 'bazaar-clone-bucket',
-      acl: "public-read",
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: file.fieldname});
-      },
-      key: function (req, file, cb) {
-        cb(null, shortid.generate() + '-' + file.originalname)
-      }
+        s3: s3,
+        bucket: 'bazaar-clone-bucket',
+        acl: "public-read",
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, shortid.generate() + '-' + file.originalname)
+        }
     })
-  })
+})
 
 
 exports.requireSignin = (req, res, next) => {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1]
         const user = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = user        
-    }else{
+        req.user = user
+    } else {
         return res.status(400).json({ message: 'Authorization required.' })
     }
     next()
